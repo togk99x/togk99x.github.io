1d8ecbbc7c25d896bcc8f4799d345b5b90a57dd8
togk99x <togk99x@gmail.com>
2026-03-17 16:02:17 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-test
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('#nav a');
+
+hamburger.addEventListner('click', function () {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('active');
+  document.body.classList.toggle('no-scrol');
+});
+
+navLinks.forEach(link => {
+  link.addEventListner('click', function () {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
+});
